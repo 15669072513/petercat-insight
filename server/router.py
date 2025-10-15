f778859936206073449ded2a4163e08ae82bb157
15669072513 <liuhq156@antgroup.com>
2025-10-15 10:33:05 +0800

    add github token

--- server/router.py
+++ server/router.py
@@ -134,6 +134,7 @@ def get_clomonitor_lint(gitUrl: str):
         # 从URL中提取仓库名称
         try:
             # 处理https://github.com/owner/repo格式
+            gitUrl = gitUrl.replace('.git', '')
             path_parts = gitUrl.replace('https://github.com/', '').replace('.git', '').split('/')
             if len(path_parts) >= 2:
                 repo = path_parts[1]
@@ -184,7 +185,6 @@ def get_clomonitor_lint(gitUrl: str):
             }
 
         # 构造clomonitor命令
-        gitUrl = gitUrl.replace('.git', '')
         linter_cmd = [
             LINTER_EXECUTABLE,
             "--mode", MODE,
